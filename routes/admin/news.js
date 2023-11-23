6fd14db21621a36be221b76544da728f87e0a65e
Walter Manqui <wmanqui@gmail.com>
2023-11-22 23:54:16 -0300

    modificación de archivo login.hbs

--- routes/admin/news.js
+++ routes/admin/news.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var newsModel = require('../../models/newsModel');
 
+///*
 router.get('/', function(req , res, next){
     res.render('admin/news',{
         layout: 'admin/layout',
@@ -9,3 +11,18 @@ router.get('/', function(req , res, next){
 });
 
 module.exports = router;
+//*/
+
+/*
+router.get('/', async function(req , res, next){
+    var news =  await newsModel.getNovedades();
+
+    res.render('admin/news',{
+        layout: 'admin/layout',
+        usuario: req.session.nombre,
+        novedades
+        });
+});
+
+module.exports = router;
+*/
