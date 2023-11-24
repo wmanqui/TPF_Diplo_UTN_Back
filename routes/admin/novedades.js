f7225ddb07754ca4fbda08ff2f34a9094b71905b
Walter Manqui <wmanqui@gmail.com>
2023-11-24 10:12:21 -0300

    Se modificaron los archivos novedades.js y agregar hbs para poder agregar datos en la BD

--- routes/admin/novedades.js
+++ routes/admin/novedades.js
@@ -21,4 +21,26 @@ router.get('/agregar',(req, res, next) =>{
 }); //cierro Get
 
 
+//Este controlador es necesario para capturar los datos enviados por el usuario,
+//los valida y los inserta en la base de datos
+router.post('/agregar', async (req, res, next) => {
+    try{
+        if(req.body.titulo != "" && req.body.subtitulo != "" && req.body.cuerpo != ""){
+            await novedadesModel.insertNovedad(req.body);
+            res.redirect('/admin/novedades')
+        }else{
+            res.render('admin/agregar',{
+                layout: 'admin/layout',
+                error: true,message: 'Todos los campos son requeridos'
+            })
+        }
+    }catch(error){
+        console.log(error)
+        res.render('admin/agregar',{
+            layout: 'admin/layout',
+            error: true, message: 'No se cargo la novedad'
+        });
+    }
+}); 
+
 module.exports = router;
