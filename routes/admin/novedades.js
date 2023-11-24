0c9214af7e8d83e7c30c4c67be4f21ebeab5855d
Walter Manqui <wmanqui@gmail.com>
2023-11-24 00:05:18 -0300

    Se comenzo a crear codigo para el alta de novedades

--- routes/admin/novedades.js
+++ routes/admin/novedades.js
@@ -2,21 +2,10 @@ var express = require('express');
 var router = express.Router();
 var newsModel = require('../../models/novedadesModel');
 
-/*
-router.get('/', function(req , res, next){
-    res.render('admin/news',{
-        layout: 'admin/layout',
-        usuario: req.session.nombre,
-        });
-});
 
-module.exports = router;
-*/
-
-///*
+//Este controlador nos permite listar la novedades
 router.get('/', async function(req , res, next){
     var novedades =  await newsModel.getNovedades();
-
         res.render('admin/novedades', {
             layout: 'admin/layout',
             usuario: req.session.nombre,
@@ -24,5 +13,12 @@ router.get('/', async function(req , res, next){
         });
 });
 
+//Este controlaador nos permite ver el formulario de alta
+router.get('/agregar',(req, res, next) =>{
+    res.render('admin/agregar',{
+        layout: 'admin/layout'
+    }); //cierro render
+}); //cierro Get
+
+
 module.exports = router;
-//*/
