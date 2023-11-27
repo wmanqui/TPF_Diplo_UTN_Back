faa48c16385e8eddb1edc5920c5099a5df51753a
Walter Manqui <wmanqui@gmail.com>
2023-11-27 16:42:11 -0300

    Se genero codigo en  el archivo api.js  y el archivo .env, para poder realizar el envio de email desde React

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,36 @@
 var express = require('express');
 var router = express.Router();
 var newsModel = require('./../models/novedadesModel');
+var nodemailer = require('nodemailer');
+
+
 
 router.get('/novedades',async function (req,res,next){
     let novedades = await newsModel.getNovedades();
     res.json(novedades);
+});
+
+router.post('/contacto', async (req, res) => {
+    const mail = {
+        to: 'wmanqui@gmail.com',
+        subject: 'Contacto web',
+        html: `${req.body.nombre} se contacto a traves de la web y quiere más información a este correo: ${req.body.email}  <br> Su telefono es ${req.body.telefono}`
+    }
+    const transport = nodemailer.createTransport({
+        host: process.env.SMTP_HOST,
+        port: process.env.SMTP_PORT,
+        auth:{
+            user: process.env.SMTP_USER,
+            pass: process.env.SMTP_PASS
+        }
+    });
+
+    await transport.sendMail(mail)
+
+    res.status(201).json({
+        error: false,
+        message: 'Mensaje enviado'
+    });
 });
 
 module.exports = router;
