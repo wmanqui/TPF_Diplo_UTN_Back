4dc065006fe4d77ee0411920392393cd79092fb6
Walter Manqui <wmanqui@gmail.com>
2023-12-03 15:02:37 -0300

    Se modifico el archivco api.js para la manipulacion de imagenes

--- routes/api.js
+++ routes/api.js
@@ -2,12 +2,37 @@ var express = require('express');
 var router = express.Router();
 var newsModel = require('./../models/novedadesModel');
 var nodemailer = require('nodemailer');
+var cloudinary = require('cloudinary').v2;
 
 //Codigo para las novedades
 router.get('/novedades',async function (req,res,next){
     let novedades = await newsModel.getNovedades();
+
+    novedades = novedades.map(novedades => {
+        if (novedades.img_id){
+            const imagen = cloudinary.url(novedades.img_id, {
+                width: 960,
+                height: 200,
+                crop:'fill'
+            });
+            return{
+                ...novedades,
+                imagen
+            }
+        }else{
+            return{
+                ...novedades,
+                imagen:''
+            }
+        }
+    });
     res.json(novedades);
 });
+
+
+
+
+
 
 //Codigo para el envio de mail
 
